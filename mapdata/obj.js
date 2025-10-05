8c71e055b434e523c42670d2d5eb712a624d2b89
Kenzie McPhie <kmcphie@uwyo.edu>
2025-10-05 12:28:57 -0600

    obj.js poit updates

--- mapdata/obj.js
+++ mapdata/obj.js
@@ -1,8 +1,8 @@
 var myStuff = {
-    "Graduate School": {city: "Fairbanks", state: "Alaska", "coord": [-147.776749, 64.835365] },
-    "Postdoc Place": {city: "Miami", state: "Florida", "coord": [-80.191788, 25.761681]},
-    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.502, 41.3104]},
-    "Favorite Vacation": {city: "San Diego", state: "California", "coord": [-117.161087, 32.715736] }
+    "Graduate School": {city: "Laramie", state: "Wyoming", "coord": [-105.579104, 41.313061] },
+    "First Fieldwork": {city: "Yellowstone", state: "Wyoming", "coord": [-110.713735, 45.032116]},
+    "Current Residence": {city: "Laramie", state: "Wyoming", "coord": [-105.619700, 41.309794]},
+    "Favorite Vacation": {city: "Boston", state: "Massachusetts", "coord": [-71.056536, 42.355835] }
 }
 
 
